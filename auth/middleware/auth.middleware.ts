61c6ab5a98d89ebb63c4c9a2ecb921d247194914
ngoctran <tranngoc897@gmail.com>
2021-08-14 18:15:26 +0700

    Add argon2 lib

--- auth/middleware/auth.middleware.ts
+++ auth/middleware/auth.middleware.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import usersService from '../../users/services/users.service';
+import * as argon2 from 'argon2';
 
 class AuthMiddleware {
     async verifyUserPassword(
@@ -12,15 +13,14 @@ class AuthMiddleware {
         );
         if (user) {
             const passwordHash = user.password;
-            return next();
-            // if (await argon2.verify(passwordHash, req.body.password)) {
-            //     req.body = {
-            //         userId: user._id,
-            //         email: user.email,
-            //         permissionFlags: user.permissionFlags,
-            //     };
-            //     return next();
-            // }
+            if (await argon2.verify(passwordHash, req.body.password)) {
+                req.body = {
+                    userId: user._id,
+                    email: user.email,
+                    permissionFlags: user.permissionFlags,
+                };
+                return next();
+            }
         }
         res.status(400).send({ errors: ['Invalid email and/or password'] });
     }
